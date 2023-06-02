a47e2a753cd9aad16170fb4dd316101d00fa0867
Robson Colaço <63608891+Robson-C@users.noreply.github.com>
2023-06-02 18:30:40 -0300

    att aulas

---
+++ Site-Base/script.js
@@ -0,0 +1,11 @@
+function abrir() {
+    menu.style.display = 'flex';
+    openMenu.style.display = 'none';
+}
+
+function fechar() {
+    menu.removeAttribute('style');
+    openMenu.removeAttribute('style');
+}
+
+
